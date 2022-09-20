b2897732c5deda384f49e2c04890c9791a5fdea5
Alexbnet <alexbnet@hotmail.co.uk>
2022-09-20 16:18:01 +0100

    Like a phoenix...

---
+++ dice.js
@@ -0,0 +1,29 @@
+var dice = {
+    sides: 6,
+    roll: function () {
+        var randomNumber = Math.floor(Math.random() * this.sides) + 1;
+        return randomNumber;
+    }
+}
+
+function printNumber(number, i) {
+    var dice = document.getElementById('dice' + String(i));
+    dice.innerHTML = number;
+}
+
+var button = document.getElementById('rolldice');
+
+var rollsmade = 0
+
+button.onclick = function() {
+    for (let j = 1; j < 6; j++) {
+        var checkbox = document.getElementById('dicecheck' + String(j)).checked
+        if (!checkbox) {
+            var result = dice.roll();
+            printNumber(result, j);
+        }
+    }
+
+    rollsmade++;
+    rollsleft.innerHTML = 3 - rollsmade;
+};
