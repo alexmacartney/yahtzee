8d3bcbaf9483c8836ae399c6e3997fbe8056a916
alexmacartney <alexmacartney@monzo.com>
2022-09-21 15:01:57 +0100

    It looks better now
    It looks better now.

--- next_round.js
+++ next_round.js
@@ -1,7 +1,7 @@
 function nextRound() {
     for (let j = 1; j < 6; j++) {
         document.getElementById('diceCheck' + String(j)).checked = 0;
-        document.getElementById('dice' + String(j)).innerHTML = 0;
+        document.getElementById('dice' + String(j)).innerHTML = '';
     }
     rollsMade = 0
     rollsLeft.innerHTML = 3
