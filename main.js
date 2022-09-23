98f51e88ae6e6c18551e9594b570f774b4764362
Alexbnet <alexbnet@hotmail.co.uk>
2022-09-23 21:40:10 +0100

    Fixed a bug

--- main.js
+++ main.js
@@ -16,11 +16,13 @@ const createWindow = () => {
 
 app.whenReady().then(() => {
   createWindow();
-})
 
-app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') app.quit();
-})
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') app.quit();
